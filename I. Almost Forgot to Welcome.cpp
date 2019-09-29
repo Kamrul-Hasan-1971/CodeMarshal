197fa07b097721aa4a23c5308e8832f9fe57e535
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2019-09-29 15:05:23 +0600

    Add files via upload

---
+++ I. Almost Forgot to Welcome.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    printf("Welcome to ICPC Dhaka Regional Online Preliminary Contest, 2019\n");
+    return 0 ;
+}
+
+//https://algo.codemarshal.org/contests/icpc-dhaka-19-preli/problems/I
